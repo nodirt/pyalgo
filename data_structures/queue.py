be84d09a6114cffeeefc78f9852550ad465322cd
nodirt <turakulov@gmail.com>
2013-06-17 23:24:04 -0400

    Queue

---
+++ data_structures/queue.py
@@ -0,0 +1,65 @@
+class Empty(Exception):
+    pass
+
+class Node(object):
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+class Queue(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def empty(self):
+        return self.head is None
+
+    def enqueue(self, item):
+        node = Node(item)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.size += 1
+
+    def dequeue(self):
+        if self.empty():
+            raise Empty()
+        item = self.head.item
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+        self.size -= 1
+        return item
+
+
+def main():
+    n = 10
+    q = Queue()
+    assert(q.empty())
+    
+    expected = range(n)
+    for i, x in enumerate(expected):
+        assert(len(q) == i)
+        q.enqueue(x)
+        assert(len(q) == i + 1)
+    
+    actual = []
+    while not q.empty():
+        actual.append(q.dequeue())
+    assert(actual == expected)
+    assert(len(q) == 0)
+    
+    print('All tests passed')
+
+
+if __name__ == '__main__':
+    main()
---
+++ data_structures/queue_using_array.py
@@ -0,0 +1,65 @@
+class Error(Exception):
+    pass
+
+class FixedSizeQueue(object):
+    def __init__(self, max_size=100):
+        self.items = [None] * (max_size + 1)
+        self.head = 0
+        self.tail = 0
+
+    def __len__(self):
+        size = self.tail - self.head
+        if size < 0:
+            size = len(self.items) - size
+        return size
+
+    def _inc(self, x):
+        x += 1
+        if x >= len(self.items):
+            x -= len(self.items)
+        return x
+
+    def empty(self):
+        return self.head == self.tail
+
+    def full(self):
+        return self._inc(self.tail) == self.head
+
+    def enqueue(self, item):
+        if self.full():
+            raise Error('Queue is full: %s' % len(self.items))
+        self.items[self.tail] = item
+        self.tail = self._inc(self.tail)
+
+    def dequeue(self):
+        if self.empty():
+            raise Error('Queue is empty')
+        item = self.items[self.head]
+        self.items[self.head] = None
+        self.head = self._inc(self.head)
+        return item
+
+
+def main():
+    n = 10
+    q = FixedSizeQueue(max_size=n)
+    assert(q.empty())
+
+    expected = range(n)
+    for i, x in enumerate(expected):
+        assert(len(q) == i)
+        q.enqueue(x)
+        assert(len(q) == i + 1)
+    assert(q.full())
+
+    actual = []
+    while not q.empty():
+        actual.append(q.dequeue())
+    assert(actual == expected)
+    
+    print('All tests passed')
+
+
+if __name__ == '__main__':
+    main()
+
